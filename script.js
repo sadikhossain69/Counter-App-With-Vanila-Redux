ac63e95747e14556de618bfcf6cd33c89b7bc15b
sadikhossain69 <sadikhossain433@gmail.com>
2022-08-13 05:35:52 +0600

    initial state created

---
+++ script.js
@@ -0,0 +1,20 @@
+// initial state
+const initialState = {
+    value: 0,
+}
+
+// create reducer function
+// function counterReducer(state = initialState, action) {
+//     switch (action.type) {
+//         case 'INCREMENT':
+//             return {
+//                 value: state.value + 1,
+//             }
+//         case 'DECREMENT':
+//             return {
+//                 value: state.value - 1,
+//             }
+//         default:
+//             return state
+//     }
+// }
